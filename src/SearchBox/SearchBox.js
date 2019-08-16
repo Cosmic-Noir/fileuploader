068d5cf69019690bc8ed8d63ee1d22e5b96c1082
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-15 19:55:44 -0600

    Add functionality for filtering via search input or radio selection with event handlers

--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -7,7 +7,11 @@ class SearchBox extends React.Component {
     return (
       <div className="SearchBox">
         <FontAwesomeIcon icon={faSearch} />
-        <input placeholder="Search term" />
+        <input
+          placeholder="Search term"
+          value={this.props.searchTerm}
+          onChange={e => this.props.handleUpdate(e.target.value)}
+        />
       </div>
     );
   }
